315b56e85e275695716ef1a4f75c272c0bd13442
FilipRebizant <filiprebizant@gmail.com>
2019-11-13 19:29:28 +0100

    added dummy data

--- docker/mariadb/sql/createDatabase.sql
+++ docker/mariadb/sql/createDatabase.sql
@@ -5,3 +5,28 @@ CREATE OR REPLACE TABLE MainTable (
   `City` varchar(50) default NULL,
   PRIMARY KEY (`id`)
 );
+
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Kenneth','40','Edina');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Hop','33','Alexandria');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Neville','46','San Antonio');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Brady','30','Perth Amboy');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Bruno','46','Sturgis');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Kyle','19','Shelton');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Hunter','43','Chesapeake');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Prescott','38','Clarksville');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Perry','21','Spartanburg');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Burke','18','Butler');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Burke','37','San Juan Capistrano');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Graiden','20','Eugene');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Guy','43','Carson');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Christian','47','Lomita');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Thomas','19','Hastings');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Phelan','39','Irwindale');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Kirk','21','Covina');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Paki','47','Scranton');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Uriah','39','Vallejo');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Quamar','44','Wahoo');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Timon','48','Mankato');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Murphy','29','Muncie');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Xavier','43','Hannibal');
+INSERT INTO `MainTable` (`Name`,`Age`,`City`) VALUES ('Blaze','49','Egg Harbor');
