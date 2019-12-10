29087438ebde1e4f3c04a569565aa14030175b46
Rejman <rreejjmmaann@.gmail.com>
2019-12-10 16:03:23 +0100

    Add clickhouse doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
@@ -25,6 +26,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $columns = $this->mariaDBRepository->getColumnNames();
+
 
         return $this->render('home/index.html.twig', [
             'columns' => $columns,
